cee21227862820a5b3e73fe7d4376b4048f507e5
reynaldojrtan29 <rtan@wela.online>
2023-01-10 10:57:16 +0800

    add vercel adapter

--- src/db/auth/auth.js
+++ src/db/auth/auth.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import supabase from "$db/database/supabase";
 import userDataStore from "../../stores/userData";
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     kit: {
-        adapter: adapter(),
+        adapter: adapter({
+            edge: false,
+        }),
         alias: {
             $db: "./src/db",
             $funcstions: "./src/functions",
